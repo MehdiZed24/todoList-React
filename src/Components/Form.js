0f7c35a3efd2d81fea5ebf7bacffd7963dff2fe9
MehdiZed24 <mehdiziani1989@gmail.com>
2022-01-11 15:44:52 +0100

    installation de uuid + Suppresion des taches Todo

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,23 +1,42 @@
 import { useState } from "react";
 import Item from "./Item";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Form() {
+  const [dataArr, setDataArr] = useState([
+    { txt: "Apprendre react", id: uuidv4() },
+    { txt: "Sport", id: uuidv4() },
+    { txt: "Apprendre PHP", id: uuidv4() },
+  ]);
+
+  const deleteElement = (id) => {
+const filteredState = dataArr.filter(item=>{
+  return item.id !==id;
+})
+setDataArr(filteredState)
+  };
+
   return (
     <div className="m-auto px-4 col-12 col-sm-10 col_lg-6">
       <form className="mb-3">
         <label htmlFor="todo" className="form-label mt-3">
-          Chose à faire
+          Chose à faire :
         </label>
         <input type="text" id="todo" className="form control" />
-        <button className="mt-2 btn btn-primary d-block">
-          Envoyez
-        </button>
+        <button className="mt-2 btn btn-primary d-block">Envoyez</button>
       </form>
       <h2>Liste des choses à faire : </h2>
       <ul className="list-group">
-        <Item />
-        <Item />
-        <Item />
+        {dataArr.map((item) => {
+          return (
+          <Item 
+          txt={item.txt}
+          key={item.id}
+          id ={item.id}
+          delFunc={deleteElement} 
+          />
+          )
+        })}
       </ul>
     </div>
   );
